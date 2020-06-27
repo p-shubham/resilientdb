4f38a0c0a989dfc4235741198de87411f75aedbe
Shubham Pandey <p.shubham1996@gmail.com>
2020-06-27 09:32:33 -0700

    New run python file

--- HPC/run_resilient_db.py
+++ HPC/run_resilient_db.py
@@ -2,7 +2,7 @@ import os
 import sys
 import time
 
-cpu = [15,23,25,26,27,29,37,38,38,38]
+cpu = [3,6,7,10,11]
 
 def __main__(cpu,replica=4,client=1):
     create_ifconfig(cpu,replica,client)
@@ -41,7 +41,7 @@ def create_bash_scripts(cpu, replica, client):
         lines.append("#SBATCH --nodelist=cpu-" + str(cpu[r]) + "\n")
         lines.append("#SBATCH --account=cpu-s2-moka_blox-0"+"\n")
         lines.append("#SBATCH --partition=cpu-s2-core-0"+"\n")
-        lines.append("#SBATCH --reservation=cpu-s2-moka_blox-0_16"+"\n")
+        lines.append("#SBATCH --reservation=cpu-s2-moka_blox-0_18"+"\n")
         lines.append("#SBATCH --mem=2G"+"\n")
         lines.append("#SBATCH -o out" + str(r) + ".txt"+"\n")
         lines.append("#SBATCH -e err" + str(r) + ".txt"+"\n")
@@ -62,7 +62,7 @@ def create_bash_scripts(cpu, replica, client):
         lines.append("#SBATCH --nodelist=cpu-" + str(cpu[c]) + "\n")
         lines.append("#SBATCH --account=cpu-s2-moka_blox-0"+"\n")
         lines.append("#SBATCH --partition=cpu-s2-core-0"+"\n")
-        lines.append("#SBATCH --reservation=cpu-s2-moka_blox-0_16"+"\n")
+        lines.append("#SBATCH --reservation=cpu-s2-moka_blox-0_18"+"\n")
         lines.append("#SBATCH --mem=2G"+"\n")
         lines.append("#SBATCH -o out" + str(c) + ".txt"+"\n")
         lines.append("#SBATCH -e err" + str(c) + ".txt"+"\n")
@@ -81,4 +81,4 @@ if(len(sys.argv) == 3):
     print("Warning: rundb & runcl must be complied with correct values in config.h")
     __main__(cpu,int(sys.argv[1]),int(sys.argv[2]))
 else:
-    print("Command line arguments absent or invalid, please retry. \n Format: python run_resilient_db <replica> <client> <username>")
+    print("Command line arguments absent or invalid, please retry. \n Format: python run_resilient_db <replica> <client>")
